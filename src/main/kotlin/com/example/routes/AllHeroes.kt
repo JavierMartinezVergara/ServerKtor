d7ba5a4368a0ea4d42f3c17a964d7e41fee6bd30
Javier Martinez <javierrrr.15@gmail.com>
2022-08-17 13:21:44 -0500

    Repository Impl Added

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,33 +1,41 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
+import io.ktor.http.HttpStatusCode.Companion
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 
 fun Route.getAllHeroes() {
-    get("/boruto/heroes") {
-        try {
-            val page = call.request.queryParameters["page"]?.toInt() ?: 1
-            require(page in 1..5)
-            call.respond(message = page)
-        } catch (e: NumberFormatException) {
-            call.respond(
-                message = ApiResponse(
-                    success = false,
-                    message = "Only Numbers Allowed"
-                ),
-                status = HttpStatusCode.BadRequest
-            )
-        } catch (e: IllegalArgumentException) {
-            call.respond(
-                message = ApiResponse(
-                    success = false,
-                    message = "Hero Not Found"
-                ),
-                status = HttpStatusCode.NotFound
-            )
-        }
+  val heroRepository: HeroRepository by inject()
+  get("/boruto/heroes") {
+    try {
+      val page = call.request.queryParameters["page"]?.toInt() ?: 1
+      require(page in 1..5)
+      val apiResponse = heroRepository.getAllHeroes(page)
+      call.respond(
+        message = apiResponse,
+        status = HttpStatusCode.OK
+      )
+    } catch (e: NumberFormatException) {
+      call.respond(
+        message = ApiResponse(
+          success = false,
+          message = "Only Numbers Allowed"
+        ),
+        status = HttpStatusCode.BadRequest
+      )
+    } catch (e: IllegalArgumentException) {
+      call.respond(
+        message = ApiResponse(
+          success = false,
+          message = "Hero Not Found"
+        ),
+        status = HttpStatusCode.NotFound
+      )
     }
+  }
 }
